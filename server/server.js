b0145bdefaa07fcd27775e19e9daa79320f86ce7
shubhampawale7 <shubhampawale7@gmail.com>
2025-07-01 00:23:56 +0530

    fix: Correct route handling for serving frontend index.html in production

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/dist")));
 
   // For any route that is not an API route, serve the frontend's index.html
-  app.get("*", (req, res) =>
+  app.get("/*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
   );
 } else {
