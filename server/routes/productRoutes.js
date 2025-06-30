1721c11ea5f720a902118787d90d1484405c800a
shubhampawale7 <shubhampawale7@gmail.com>
2025-07-01 01:17:25 +0530

    refactor: Combine GET and POST routes on the root path to avoid route collision

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 const router = express.Router();
+
 import {
   getProducts,
   getProductById,
@@ -9,19 +10,20 @@ import {
   createProductReview,
   getProductCategories,
 } from "../controllers/productController.js";
+
 import { protect, admin } from "../middlewares/authMiddleware.js";
 
-// Public routes
-router.route("/").get(getProducts);
+// Combine GET + POST to avoid route collision on "/"
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+
 router.route("/categories").get(getProductCategories);
-router.route("/:id").get(getProductById);
 
-// Private user route
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
+
 router.route("/:id/reviews").post(protect, createProductReview);
-
-// Admin only routes
-router.route("/").post(protect, admin, createProduct);
-router.route("/:id").put(protect, admin, updateProduct);
-router.route("/:id").delete(protect, admin, deleteProduct);
 
 export default router;
