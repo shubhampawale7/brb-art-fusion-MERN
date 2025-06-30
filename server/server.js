ed5b54fcb3439d062b3c644e7d3dd15bafe806ca
shubhampawale7 <shubhampawale7@gmail.com>
2025-07-01 00:29:50 +0530

    fix: Update server port configuration from 5000 to 4000

--- server/server.js
+++ server/server.js
@@ -56,6 +56,6 @@ if (process.env.NODE_ENV === "production") {
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
