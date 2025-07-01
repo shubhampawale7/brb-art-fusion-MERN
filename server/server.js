10ba851b610d5c756503acf476fa20e246aff08c
shubhampawale7 <shubhampawale7@gmail.com>
2025-07-01 12:42:04 +0530

    refactor: Update route handling for production environment

--- server/server.js
+++ server/server.js
@@ -30,11 +30,11 @@ app.use("/api/contact", contactRoutes);
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "/client/dist")));
-  app.get("*", (req, res) =>
+  app.get("/(.*)/", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
   );
 } else {
-  app.get("/*splat", (req, res) => {
+  app.get("/", (req, res) => {
     res.send("API is running...");
   });
 }
