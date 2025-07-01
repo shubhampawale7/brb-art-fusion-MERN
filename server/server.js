f220e4f03c2c57ce991190764f240da1b31a9600
shubhampawale7 <shubhampawale7@gmail.com>
2025-07-01 12:52:16 +0530

    refactor: Update route patterns for user, product, order, upload, wishlist, article, and contact routes

--- server/server.js
+++ server/server.js
@@ -19,18 +19,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/users", userRoutes);
-app.use("/api/products", productRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/upload", uploadRoutes);
-app.use("/api/wishlist", wishlistRoutes);
-app.use("/api/articles", articleRoutes);
-app.use("/api/contact", contactRoutes);
+app.use("/api/*users", userRoutes);
+app.use("/api/*products", productRoutes);
+app.use("/api/*orders", orderRoutes);
+app.use("/api/*upload", uploadRoutes);
+app.use("/api/*wishlist", wishlistRoutes);
+app.use("/api/*articles", articleRoutes);
+app.use("/api/*contact", contactRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "/client/dist")));
-  app.get("/(.*)/", (req, res) =>
+  app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
   );
 } else {
